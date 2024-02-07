130abae5e3a2152278b8df50195acd8c34f904c8
unknown <areg.sarkisyan.99@mail.ru>
2024-02-07 07:22:52 +0300

    fix: link add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
     return(
         <header className="header">
             <Link to="/scientist"><img src={Logo} alt="" className="header__logo" /></Link>
-            <a href="" className="header__link">Аудирование</a>
+            <a href="https://test-english.com/listening/b1/biography-albert-einstein-listening-test/" className="header__link">Аудирование</a>
         </header>
     )
 }
